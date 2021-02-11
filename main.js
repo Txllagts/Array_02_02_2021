e2648afb52aa466d3809a1d6b506114fee25c686
Jasbeith Andrea Prada Hernández <jasbeith2505@gmail.com>
2021-02-11 18:09:39 -0500

    Array_02_02_2021

---
+++ main.js
@@ -0,0 +1,22 @@
+let lista = ["miguel","diana","diego","sergio","wilmer","adrian","andres","emmanuel","loza","andrea"];
+
+//let resultado = lista.pop();
+//let resultado = lista.push(100);
+//let resultado = lista.shift();
+//let resultado = lista.unshift({nombre:'profesor'});
+//let resultado = lista.splice(6,2,"uva", "manzana");
+//let resultado = lista.sort((a,b)=>{
+    //console.log(b);
+//});
+//let resultado = lista.some((data, i, lista) => {
+    //console.log(i);
+    //console.log(data);
+    //console.log(lista);
+    //console.log(`indice ${i} valor ${data} condicion ${data=="adrian"}`);
+//});
+
+
+console.log(`Lista original`);
+console.log(lista);
+console.log(`Lista afectada`);
+console.log(resultado);
